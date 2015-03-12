4de678e02b9bd5a2917c22cd487fd95b0d9f26eb
jlouistsg <Jlouis@MacBook-Air-de-Jlouis.local>
2015-03-12 22:24:28 +0100

    Fix Sonata Admin
    - Update config to load the admin.xml of the bundle
    - load the admin.xml into the bundle

--- src/Esgi/BlogBundle/DependencyInjection/EsgiBlogExtension.php
+++ src/Esgi/BlogBundle/DependencyInjection/EsgiBlogExtension.php
@@ -23,6 +23,7 @@ class EsgiBlogExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('admin.xml');
         $loader->load('services.xml');
     }
 }
