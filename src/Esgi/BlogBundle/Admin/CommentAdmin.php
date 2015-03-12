08c37af826e6aed1cfb3494bf9dbb690cb0519bb
jlouistsg <Jlouis@MacBook-Air-de-Jlouis.local>
2015-03-12 22:45:19 +0100

    Add different entity on Sonata
    - Add Category
    - Add Comment

---
+++ src/Esgi/BlogBundle/Admin/CommentAdmin.php
@@ -0,0 +1,43 @@
+<?php
+namespace Esgi\BlogBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CommentAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', array('label' => 'Post Title'))
+            ->add('content')
+            ->add('isPublished')
+            ->add('created')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('content')
+            ->add('isPublished')
+            ->add('created')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('content')
+            ->add('isPublished')
+            ->add('created')
+        ;
+    }
+}
